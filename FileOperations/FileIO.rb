06e8e4fa14861459ba77951dabfa963f71ecce2b
chetan K S <chetan.ks@v2solutions.com>
2015-08-18 09:31:46 +0530

    Optimized the code by removing extra used array and method

--- FileOperations/FileIO.rb
+++ FileOperations/FileIO.rb
@@ -6,54 +6,40 @@ class FileIO
 		@day = []
 	end
 
-	def readFile
-		
-		file = File.open("/home/chetan/Downloads/weather.txt")
-		parse_collected_data(file)
-	end
-
 	def parse_collected_data(file)
 
-		min_temp = []
-		max_temp = []
+		max_min_temp = []
 		temp_spread = []
 
 		file.each_line do |line|
-		@day << line.split(" ").values_at(0)
-		max_temp << line.split(" ").values_at(1)
-		min_temp << line.split(" ").values_at(2)
+		max_min_temp << line.split(" ").values_at(0..2)
 		
-		end
-		
-		@day = @day.drop(2)
-		min_temp = min_temp.drop(2)
-		max_temp = max_temp.drop(2)
-		ary_length = @day.length
-		
-		ary_length.times do |index|
-			@day[index] = @day[index][0].to_i
-			max_temp[index] = max_temp[index][0].to_f
-			min_temp[index] = min_temp[index][0].to_f
-			temp_spread[index] = max_temp[index] - min_temp[index]
 		end
 
-		find_min_tempSpread(temp_spread)
+		max_min_temp = max_min_temp.drop(2)
+		
+		max_min_temp.length.times do |index|
+			@day[index] = max_min_temp[index][0].to_i
+			temp_spread[index] = max_min_temp[index][1].to_f - max_min_temp[index][2].to_f
+		end
+
+		find_day_minTempSpread(temp_spread)
 
 	end
 
-	def find_min_tempSpread(temp_spread)
+	def find_day_minTempSpread(temp_spread)
 
-		least_temp = temp_spread.min
-		index = temp_spread.index(least_temp)
+		index = temp_spread.index(temp_spread.min)
 		@day.at(index)
+	
 	end
 end
 
-FileIO.new.readFile
+FileIO.new.parse_collected_data(File.open("/home/chetan/Downloads/weather.txt"))
 
 class FilTest < Test::Unit::TestCase
 
 	def test_dayNumber
-		assert_equal(14, FileIO.new.readFile)	
+		assert_equal(14, FileIO.new.parse_collected_data(File.open("/home/chetan/Downloads/weather.txt")))	
 	end
 end
