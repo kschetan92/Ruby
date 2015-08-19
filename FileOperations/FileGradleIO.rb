c522db488a645dbe90f6ec70827180d9f9563f45
chetan K S <chetan.ks@v2solutions.com>
2015-08-19 18:18:31 +0530

    removing cryptic variables and optimizing the code

--- FileOperations/FileGradleIO.rb
+++ FileOperations/FileGradleIO.rb
@@ -1,26 +1,18 @@
-
 class FileGradleIO
 
-	def initialize(path_array)
-		0.upto(path_array.length-1) do |i|
-			@file = path_array[i]
-			@file_read = File.read(@file)
-			parse_collected_data("DEV", "DEVCode", "DEVName")
+	def initialize(pathArray)
+		0.upto(pathArray.length-1) do |i|
+			@file = pathArray[i]
+			@file_Read = File.read(@file)
+			parseCollectedData("DEV", "1111", "SNAP")
 		  end
 	end
 
-	def parse_collected_data(section, versionCode, versionName)
+	def parseCollectedData(section, versionCode, versionName)
 
-		mch_VCode = /#{section}.*?versionCode( )*(=)*( )*(\S+)/m
-		mch_VName = /#{section}.*?versionName( )*(=)*( )*(\W)(\S+)(\S)/m
-
-		mch_VCode.match(@file_read)
-		replaced = @file_read.sub!("#{$4}", versionCode)
-		File.open(@file,"w") {|file| file.write replaced}
-
-		mch_VName.match(@file_read)
-		replaced = @file_read.sub!("#{$5}", versionName)
-		File.open(@file,"w") {|file| file.write replaced}
+		@file_Read.sub!(/#{section}.*?versionCode( )*(=)*( )*(\S+)/m) { |ma| ma.sub!($4, versionCode) }
+		@file_Read.sub!(/#{section}.*?versionName( )*(=)*( )*(\W)(\S+)(\S)/m) { |ma| ma.sub!($5, versionName) }
+		File.open(@file,"w") {|file| file.write @file_Read}
 
 	end
 end
